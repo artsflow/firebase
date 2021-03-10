5f1a56bf41f1c39a464032dde2c27997830b3eb3
ciocan <ciocan@gmail.com>
2021-03-10 08:53:03 +0000

    add beta tester check

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -5,12 +5,19 @@ import { db } from './config'
 export const createUserRecord = functions
   .region('europe-west2')
   .auth.user()
-  .onCreate((user, context) => {
+  .onCreate(async (user, context) => {
     const userRef = db.doc(`users/${user.uid}`)
     const profileRef = db.doc(`profiles/${user.uid}`)
     const batch = db.batch()
 
     const [firstName = null, lastName = null] = user.displayName?.split(' ') || ''
+
+    if (!user?.email) return Promise.reject(new Error('not_auth'))
+
+    const betaTesterRef = db.collection('betatesters').doc(user.email)
+    const doc = await betaTesterRef.get()
+
+    const isBetaTester = doc.exists ? true : false
 
     const userData = {
       createdAt: context.timestamp,
@@ -20,6 +27,7 @@ export const createUserRecord = functions
       lastName,
       emailVerified: user.emailVerified,
       provider: user.providerData[0].providerId,
+      isBetaTester,
     }
 
     const profileData = {
